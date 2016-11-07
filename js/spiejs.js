50a6374243922f39fc1ffd15c77c4b626bec8b7a
Debasmita Banerjee <write2debasmita@gmail.com>
2016-11-07 23:05:22 +0530

    Update spiejs.js

--- js/spiejs.js
+++ js/spiejs.js
@@ -1,5 +1,5 @@
 function upContent(){
-	document.getElementById("details0").style.visibility="visible";
-	document.getElementById("details1").style.visibility="visible";
-	document.getElementById("details2").style.visibility="visible";
+	document.getElementById("details0").style.display='block';
+	document.getElementById("details1").style.display='block';
+	document.getElementById("details2").style.visibility='block';
 }
