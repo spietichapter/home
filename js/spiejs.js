e838bcb43f4d4d7e92bbaf150398d3972ef70178
Debasmita Banerjee <write2debasmita@gmail.com>
2016-11-07 22:31:20 +0530

    Create spiejs.js

---
+++ js/spiejs.js
@@ -0,0 +1,5 @@
+function upContent(){
+	document.getElementById("details0").style.visibility="visible";
+	document.getElementById("details1").style.visibility="visible";
+	document.getElementById("details2").style.visibility="visible";
+}
