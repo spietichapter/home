95b45d7ab011c2342ec6cbacc99f42fc1e2a0089
Debasmita Banerjee <write2debasmita@gmail.com>
2016-11-08 11:16:42 +0530

    Update spiejs.js

--- js/spiejs.js
+++ js/spiejs.js
@@ -1,9 +1,15 @@
 function upContent(){
 	document.getElementById("details0").style.display='block';
+	document.getElementById("details1").style.display='none';
+	document.getElementById("details2").style.display='none';
 }
 function upContent1(){
 	document.getElementById("details1").style.display='block';
+	document.getElementById("details0").style.display='none';
+	document.getElementById("details2").style.display='none';
 }
 function upContent2(){
 	document.getElementById("details2").style.display='block';
+	document.getElementById("details1").style.display='none';
+	document.getElementById("details0").style.display='none';
 }
