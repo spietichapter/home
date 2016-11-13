804c0edf53e1a83411afe0035067d277233fbb9a
Debasmita Banerjee <write2debasmita@gmail.com>
2016-11-13 14:49:56 +0530

    Update spiedefjs.js

--- js/spiedefjs.js
+++ js/spiedefjs.js
@@ -25,31 +25,6 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-function nomore{
-  if (slideIndex == 1 )
-  {document.getElementById("a").style.display='block';
-	document.getElementById("b").style.display='none';
-document.getElementById("c").style.display='none';
-	document.getElementById("d").style.display='none';
-  }
-   if (slideIndex == 2 )
-  {document.getElementById("b").style.display='block';
-	document.getElementById("a").style.display='none';
-document.getElementById("c").style.display='none';
-	document.getElementById("d").style.display='none';
-  }
-   if (slideIndex == 3 )
-  {document.getElementById("c").style.display='block';
-	document.getElementById("a").style.display='none';
-document.getElementById("b").style.display='none';
-	document.getElementById("d").style.display='none';
-  }
-   if (slideIndex == 4  )
-  {document.getElementById("d").style.display='block';
-	document.getElementById("b").style.display='none';
-document.getElementById("c").style.display='none';
-	document.getElementById("a").style.display='none';
-  }
-}
+
 
     
